cf7c25c74fb1338b06c4499b854e4d2cf5a83e3c
pader.zhang <pader.zhang@starlight-sms.com>
2020-02-12 13:58:51 +0800

    Refactor unit test

--- src/test/java/com/sms/satp/ApplicationTests.java
+++ src/test/java/com/sms/satp/ApplicationTests.java
@@ -1,14 +1,12 @@
 package com.sms.satp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 
 
 @TestConfiguration
-@EnableAutoConfiguration(exclude = {EmbeddedMongoAutoConfiguration.class})
 public class ApplicationTests {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SatpApplication.class, args);
