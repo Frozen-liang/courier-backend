4598827d397ea9e88ecf69c89e13eda887264f02
xiangzhen.xiong <xiangzhen.xiong@starlight-sms.com>
2020-04-02 17:04:43 +0800

    Handling test failures

--- src/test/java/com/sms/satp/controller/CommonControllerTest.java
+++ src/test/java/com/sms/satp/controller/CommonControllerTest.java
@@ -5,19 +5,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.sms.satp.ApplicationTests;
 import com.sms.satp.common.constant.Constants;
 import com.sms.satp.common.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@WebMvcTest(value = CommonController.class)
+@SpringBootTest(classes = ApplicationTests.class)
+@AutoConfigureMockMvc
 @DisplayName("Tests for CommonControllerTest")
 class CommonControllerTest {
 
