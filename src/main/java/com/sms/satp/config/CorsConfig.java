0196823d51a7214ead8ea393b29f814b05301619
xiangzhen.xiong <xiangzhen.xiong@starlight-sms.com>
2020-04-10 15:43:02 +0800

    #32 Add cross-domain settings

---
+++ src/main/java/com/sms/satp/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.sms.satp.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConfigurationProperties(prefix = "api.cors")
+@Data
+public class CorsConfig implements WebMvcConfigurer {
+
+    private String[] origins = {"*"};
+    private String[] methods = {"*"};
+    private String mapping = "/**";
+    private Long maxAge = 3600L;
+    private Boolean allowCredentials = true;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(mapping)
+            .allowedOrigins(origins)
+            .allowedMethods(methods)
+            .maxAge(maxAge)
+            .allowCredentials(allowCredentials);
+    }
+}
