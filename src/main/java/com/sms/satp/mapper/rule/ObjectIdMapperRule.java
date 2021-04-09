a6f51d788ef233b00074bf1111100689094fa666
wenqiang <wenqiang.yin@starlight-sms.com>
2021-04-09 15:20:23 +0800

    modify ObjectIdMapperRule.java

--- src/main/java/com/sms/satp/mapper/rule/ObjectIdMapperRule.java
+++ src/main/java/com/sms/satp/mapper/rule/ObjectIdMapperRule.java
@@ -1,17 +1,21 @@
 package com.sms.satp.mapper.rule;
 
 import java.util.Objects;
+import org.apache.commons.lang3.StringUtils;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ObjectIdMapperRule {
 
+    private final static String EMPTY = "";
+    private final static int OBJECT_ID_LENGTH = 24;
+
     public String asId(ObjectId objectId) {
-        return Objects.isNull(objectId) ? "" : objectId.toString();
+        return Objects.isNull(objectId) ? EMPTY : objectId.toString();
     }
 
     public ObjectId asId(String id) {
-        return new ObjectId(id);
+        return StringUtils.isNotEmpty(id) && id.length() == OBJECT_ID_LENGTH ? new ObjectId(id) : null;
     }
 }
