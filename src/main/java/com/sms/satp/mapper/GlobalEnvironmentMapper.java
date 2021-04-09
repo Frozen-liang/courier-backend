6f740d4c9bdecd6d8b8d62f3b69d206cbb4964b1
wenqiang <wenqiang.yin@starlight-sms.com>
2021-04-09 15:52:46 +0800

    delete ObjectIdMapperRule.java

--- src/main/java/com/sms/satp/mapper/GlobalEnvironmentMapper.java
+++ src/main/java/com/sms/satp/mapper/GlobalEnvironmentMapper.java
@@ -2,20 +2,20 @@ package com.sms.satp.mapper;
 
 import com.sms.satp.entity.dto.GlobalEnvironmentDto;
 import com.sms.satp.entity.env.GlobalEnvironment;
-import com.sms.satp.mapper.rule.ObjectIdMapperRule;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-    uses = ObjectIdMapperRule.class)
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface GlobalEnvironmentMapper {
 
     @Mapping(target = "createDateTime", source = "createDateTime", dateFormat = "yyyy-MM-dd HH:mm:ss")
     @Mapping(target = "modifyDateTime", source = "modifyDateTime", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    @Mapping(target = "id", expression = "java(globalEnvironment.getId().toString())")
     GlobalEnvironmentDto toDto(GlobalEnvironment globalEnvironment);
 
     @Mapping(target = "createDateTime", ignore = true)
     @Mapping(target = "modifyDateTime", ignore = true)
+    @Mapping(target = "id", expression = "java(com.sms.satp.utils.ObjectIdConverter.toObjectId(globalEnvironmentDto.getId()))")
     GlobalEnvironment toEntity(GlobalEnvironmentDto globalEnvironmentDto);
 }
