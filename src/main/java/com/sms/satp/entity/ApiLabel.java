39e2909e1a19ee214e0e5dbdd4890f791fb0bea6
wenqiang <wenqiang.yin@starlight-sms.com>
2021-04-13 10:07:36 +0800

    modify ApiLabel filed

--- src/main/java/com/sms/satp/entity/ApiLabel.java
+++ src/main/java/com/sms/satp/entity/ApiLabel.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,13 +16,9 @@ import org.springframework.data.mongodb.core.mapping.Field;
 public class ApiLabel {
 
     @Id
-    @Field("_id")
     private ObjectId id;
-    @Field("project_id")
     private String projectId;
-    @Field("label_name")
     private String labelName;
     /*1:apiLabel 2:apiCaseLabel 3:apiCasePipelineLabel*/
-    @Field("label_type")
     private Short labelType;
 }
