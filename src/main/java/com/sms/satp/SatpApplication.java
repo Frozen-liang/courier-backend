df92781774c9e7569b15021803e100874a5397c5
wenqiang <wenqiang.yin@starlight-sms.com>
2021-05-08 13:56:40 +0800

    repair code

--- src/main/java/com/sms/satp/SatpApplication.java
+++ src/main/java/com/sms/satp/SatpApplication.java
@@ -1,8 +1,5 @@
 package com.sms.satp;
 
-import com.sms.satp.dto.ApiImportRequest;
-import com.sms.satp.parser.common.DocumentType;
-import com.sms.satp.service.ApiService;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +13,5 @@ public class SatpApplication {
     public static void main(String[] args) {
         SpringApplication.run(SatpApplication.class, args);
     }
-
 
 }
