fca63a4c172c3449fb56ae6b60a8369790e454c9
wenqiang <wenqiang.yin@starlight-sms.com>
2021-05-11 11:12:39 +0800

    modify ApiMapper

--- src/main/java/com/sms/satp/mapper/ApiMapper.java
+++ src/main/java/com/sms/satp/mapper/ApiMapper.java
@@ -15,31 +15,36 @@ import org.mapstruct.ReportingPolicy;
     unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = ParamInfoMapper.class)
 public interface ApiMapper {
 
-    @Mapping(target = "apiProtocol", expression = "java(apiEntity.getApiProtocol().getCode())")
-    @Mapping(target = "requestMethod", expression = "java(apiEntity.getRequestMethod().getCode())")
-    @Mapping(target = "apiRequestParamType", expression = "java(apiEntity.getApiRequestParamType().getCode())")
-    @Mapping(target = "apiStatus", expression = "java(apiEntity.getApiStatus().getCode())")
-    @Mapping(target = "apiResponseJsonType", expression = "java(apiEntity.getApiResponseJsonType().getCode())")
-    @Mapping(target = "apiRequestJsonType", expression = "java(apiEntity.getApiRequestJsonType().getCode())")
+    @Mapping(target = "apiProtocol",
+        expression = "java(apiEntity.getApiProtocol().getCode())")
+    @Mapping(target = "requestMethod",
+        expression = "java(apiEntity.getRequestMethod().getCode())")
+    @Mapping(target = "apiRequestParamType",
+        expression = "java(apiEntity.getApiRequestParamType().getCode())")
+    @Mapping(target = "apiStatus",
+        expression = "java(apiEntity.getApiStatus().getCode())")
+    @Mapping(target = "apiResponseJsonType",
+        expression = "java(apiEntity.getApiResponseJsonType().getCode())")
+    @Mapping(target = "apiRequestJsonType",
+        expression = "java(apiEntity.getApiRequestJsonType().getCode())")
     ApiResponseDto toDto(ApiEntity apiEntity);
 
     List<ApiResponseDto> toDtoList(List<ApiEntity> apiEntityList);
 
     @Mapping(target = "createDateTime", ignore = true)
     @Mapping(target = "modifyDateTime", ignore = true)
-    @Mapping(target = "apiProtocol", expression = "java(com.sms.satp.common.enums.ApiProtocol"
-        + ".getType(apiRequestDto.getApiProtocol()))")
-    @Mapping(target = "requestMethod", expression = "java(com.sms.satp.common.enums.RequestMethod"
-        + ".getType(apiRequestDto.getRequestMethod()))")
-    @Mapping(target = "apiRequestParamType", expression = "java(com.sms.satp.common.enums.ApiRequestParamType"
-        + ".getType(apiRequestDto.getApiRequestParamType()))")
-    @Mapping(target = "apiStatus", expression = "java(com.sms.satp.common.enums.ApiStatus"
-        + ".getType(apiRequestDto.getApiStatus()))")
-    @Mapping(target = "apiResponseJsonType", expression = "java(com.sms.satp.common.enums.ApiJsonType"
-        + ".getType(apiRequestDto.getApiResponseJsonType()))")
-    @Mapping(target = "apiRequestJsonType", expression = "java(com.sms.satp.common.enums.ApiJsonType"
-        + ".getType(apiRequestDto.getApiRequestJsonType()))")
+    @Mapping(target = "apiProtocol",
+        expression = "java(com.sms.satp.common.enums.ApiProtocol.getType(apiRequestDto.getApiProtocol()))")
+    @Mapping(target = "requestMethod",
+        expression = "java(com.sms.satp.common.enums.RequestMethod.getType(apiRequestDto.getRequestMethod()))")
+    @Mapping(target = "apiRequestParamType",
+        expression = "java(com.sms.satp.common.enums.ApiRequestParamTypegetType(apiRequestDto.getApiRequestParamType()))")
+    @Mapping(target = "apiStatus",
+        expression = "java(com.sms.satp.common.enums.ApiStatus.getType(apiRequestDto.getApiStatus()))")
+    @Mapping(target = "apiResponseJsonType",
+        expression = "java(com.sms.satp.common.enums.ApiJsonType.getType(apiRequestDto.getApiResponseJsonType()))")
+    @Mapping(target = "apiRequestJsonType",
+        expression = "java(com.sms.satp.common.enums.ApiJsonType.getType(apiRequestDto.getApiRequestJsonType()))")
     ApiEntity toEntity(ApiRequestDto apiRequestDto);
-
 
 }
