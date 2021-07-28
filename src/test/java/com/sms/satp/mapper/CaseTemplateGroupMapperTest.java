42cd637a39f114fa25b2d80727e603a12428b065
shiyue.wei <shiyue.wei@starlight-sms.com>
2021-07-28 14:08:20 +0800

    Optimized mapper interface unit test

--- src/test/java/com/sms/satp/mapper/CaseTemplateGroupMapperTest.java
+++ src/test/java/com/sms/satp/mapper/CaseTemplateGroupMapperTest.java
@@ -1,8 +1,7 @@
 package com.sms.satp.mapper;
 
 import com.google.common.collect.Lists;
-import com.sms.satp.dto.request.AddCaseTemplateGroupRequest;
-import com.sms.satp.dto.request.UpdateCaseTemplateGroupRequest;
+import com.sms.satp.dto.request.CaseTemplateGroupRequest;
 import com.sms.satp.entity.group.CaseTemplateGroupEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,56 +19,43 @@ public class CaseTemplateGroupMapperTest {
 
     @Test
     @DisplayName("Test the toCaseTemplateGroupByAdd method in CaseTemplateGroupMapper")
-    void toCaseTemplateGroupByAddTest(){
-        AddCaseTemplateGroupRequest request=AddCaseTemplateGroupRequest.builder().build();
-        assertThat(caseTemplateGroupMapper.toCaseTemplateGroupByAdd(request)).isNotNull();
+    void toCaseTemplateGroupByAddTest() {
+        CaseTemplateGroupRequest request = CaseTemplateGroupRequest.builder().build();
+        assertThat(caseTemplateGroupMapper.toCaseTemplateGroupEntity(request)).isNotNull();
     }
 
     @Test
     @DisplayName("Test the toCaseTemplateGroupByAdd method in CaseTemplateGroupMapper is null")
-    void toCaseTemplateGroupByAddTest_isNull(){
-        assertThat(caseTemplateGroupMapper.toCaseTemplateGroupByAdd(null)).isNull();
-    }
-
-    @Test
-    @DisplayName("Test the toCaseTemplateGroupByUpdate method in CaseTemplateGroupMapper")
-    void toCaseTemplateGroupByUpdateTest(){
-        UpdateCaseTemplateGroupRequest request= UpdateCaseTemplateGroupRequest.builder().build();
-        assertThat(caseTemplateGroupMapper.toCaseTemplateGroupByUpdate(request)).isNotNull();
-    }
-
-    @Test
-    @DisplayName("Test the toCaseTemplateGroupByUpdate method in CaseTemplateGroupMapper is null")
-    void toCaseTemplateGroupByUpdateTest_isNull(){
-        assertThat(caseTemplateGroupMapper.toCaseTemplateGroupByUpdate(null)).isNull();
+    void toCaseTemplateGroupByAddTest_isNull() {
+        assertThat(caseTemplateGroupMapper.toCaseTemplateGroupEntity(null)).isNull();
     }
 
     @Test
     @DisplayName("Test the toResponseList method in CaseTemplateGroupMapper")
-    void toResponseListTest(){
-        List<CaseTemplateGroupEntity> caseTemplateGroupEntities= Lists.newArrayList();
-        assertThat(caseTemplateGroupMapper.toResponseList(caseTemplateGroupEntities)).isNotNull();
+    void toResponseListTest() {
+        List<CaseTemplateGroupEntity> caseTemplateGroupEntities = Lists.newArrayList();
+        assertThat(caseTemplateGroupMapper.toResponse(caseTemplateGroupEntities)).isNotNull();
     }
 
     @Test
     @DisplayName("Test the toResponseList method in CaseTemplateGroupMapper is null")
-    void toResponseListTest_isNull(){
-        assertThat(caseTemplateGroupMapper.toResponseList(null)).isNull();
+    void toResponseListTest_isNull() {
+        assertThat(caseTemplateGroupMapper.toResponse(null)).isNull();
     }
 
     @Test
     @DisplayName("Test the caseTemplateGroupEntityToCaseTemplateGroupResponse method in CaseTemplateGroupMapper")
-    void caseTemplateGroupEntityToCaseTemplateGroupResponseTest(){
-        CaseTemplateGroupEntity caseTemplateGroupEntity=CaseTemplateGroupEntity.builder().build();
-        assertThat(caseTemplateGroupMapper.toResponseList(Lists.newArrayList(caseTemplateGroupEntity))).isNotNull();
+    void caseTemplateGroupEntityToCaseTemplateGroupResponseTest() {
+        CaseTemplateGroupEntity caseTemplateGroupEntity = CaseTemplateGroupEntity.builder().build();
+        assertThat(caseTemplateGroupMapper.toResponse(Lists.newArrayList(caseTemplateGroupEntity))).isNotNull();
     }
 
     @Test
     @DisplayName("Test the caseTemplateGroupEntityToCaseTemplateGroupResponse method in CaseTemplateGroupMapper")
-    void caseTemplateGroupEntityToCaseTemplateGroupResponse_isNull(){
-        CaseTemplateGroupEntity dto=null;
-        List<CaseTemplateGroupEntity> caseTemplateGroups=Lists.newArrayList(dto);
-        assertThat(caseTemplateGroupMapper.toResponseList(caseTemplateGroups)).size().isEqualTo(1);
+    void caseTemplateGroupEntityToCaseTemplateGroupResponse_isNull() {
+        CaseTemplateGroupEntity dto = null;
+        List<CaseTemplateGroupEntity> caseTemplateGroups = Lists.newArrayList(dto);
+        assertThat(caseTemplateGroupMapper.toResponse(caseTemplateGroups)).size().isEqualTo(1);
     }
 
 }
