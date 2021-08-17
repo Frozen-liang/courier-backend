0c78dd71910a964e50ed02ae9ce722b20e0106ec
pader.zhang <pader.zhang@starlight-sms.com>
2021-08-17 17:56:41 +0800

    Add a serializer for Duration

--- src/main/java/com/sms/courier/utils/DurationToStringSerializer.java
+++ src/main/java/com/sms/courier/utils/DurationToStringSerializer.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import java.io.IOException;
 import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-import java.util.Objects;
-import org.springframework.boot.convert.DurationUnit;
+import org.springframework.boot.convert.ApplicationConversionService;
 
 public class DurationToStringSerializer extends JsonSerializer<Duration> {
 
@@ -15,9 +13,8 @@ public class DurationToStringSerializer extends JsonSerializer<Duration> {
     @Override
     public void serialize(Duration duration, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
         throws IOException {
-        DurationUnit durationUnit = duration.getClass().getAnnotation(DurationUnit.class);
-        ChronoUnit unit = Objects.nonNull(durationUnit) ? durationUnit.value() : ChronoUnit.MILLIS;
-        jsonGenerator.writeString(String.valueOf(duration.get(unit)));
+        jsonGenerator.writeString(
+            String.valueOf(ApplicationConversionService.getSharedInstance().convert(duration, Long.class)));
 
     }
 }
