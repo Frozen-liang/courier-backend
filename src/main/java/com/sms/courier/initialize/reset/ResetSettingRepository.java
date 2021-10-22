0cc8c2a9d648b634490f29ad03c1f17ab6ba08ab
wenqiang <wenqiang.yin@starlight-sms.com>
2021-10-22 09:21:01 +0800

    reset api MD5

---
+++ src/main/java/com/sms/courier/initialize/reset/ResetSettingRepository.java
@@ -0,0 +1,8 @@
+package com.sms.courier.initialize.reset;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ResetSettingRepository extends MongoRepository<ResetSetting, String> {
+
+   ResetSetting findByResetType(String type);
+}
