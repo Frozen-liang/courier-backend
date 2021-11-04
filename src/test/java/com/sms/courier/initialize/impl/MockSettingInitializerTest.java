61c9764395b1267215d11b5ca92415f52eba435c
wenqiang <wenqiang.yin@starlight-sms.com>
2021-11-04 16:55:24 +0800

    Modidy unit test

--- src/test/java/com/sms/courier/initialize/impl/MockSettingInitializerTest.java
+++ src/test/java/com/sms/courier/initialize/impl/MockSettingInitializerTest.java
@@ -1,19 +1,20 @@
 package com.sms.courier.initialize.impl;
 
-import com.sms.courier.entity.mock.MockSettingEntity;
-import com.sms.courier.repository.MockSettingRepository;
-import com.sms.courier.security.jwt.JwtTokenManager;
-import org.assertj.core.util.Lists;
-import org.bson.types.ObjectId;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.context.ConfigurableApplicationContext;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import com.sms.courier.entity.mock.MockSettingEntity;
+import com.sms.courier.repository.MockSettingRepository;
+import com.sms.courier.security.AccessTokenProperties;
+import com.sms.courier.security.jwt.JwtTokenManager;
+import java.util.Optional;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @DisplayName("Tests for MockSettingInitializerTest")
 public class MockSettingInitializerTest {
@@ -30,12 +31,13 @@ public class MockSettingInitializerTest {
     void init_test() {
         when(applicationContext.getBean(MockSettingRepository.class)).thenReturn(mockSettingRepository);
         when(applicationContext.getBean(JwtTokenManager.class)).thenReturn(jwtTokenManager);
-        when(mockSettingRepository.findAll()).thenReturn(null);
+        when(mockSettingRepository.findFirstByOrderByCreateDateTimeDesc()).thenReturn(Optional.empty());
+        when(applicationContext.getBean(AccessTokenProperties.class)).thenReturn(new AccessTokenProperties());
         when(jwtTokenManager.generateAccessToken(any())).thenReturn("test");
         when(mockSettingRepository.insert(any(MockSettingEntity.class)))
             .thenReturn(entity);
         mockSettingInitializer.init(applicationContext);
-        verify(mockSettingRepository,times(1)).findAll();
+        verify(mockSettingRepository, times(1)).save(any());
     }
 
 }
