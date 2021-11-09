b1140cf8ff543f667ae6fcb8289a312fb0b3a921
coco.wei <coco.wei@starlight-sms.com>
2021-11-09 10:47:32 +0800

    Repair After a scene case is deleted, the case associated with a scheduled task is also deleted

--- src/main/java/com/sms/courier/common/field/ScheduleField.java
+++ src/main/java/com/sms/courier/common/field/ScheduleField.java
@@ -6,7 +6,9 @@ public enum ScheduleField implements Field {
     NAME("name"),
     LAST_TASK_COMPLETE_TIME("lastTaskCompleteTime"),
     OPEN("isOpen"),
-    TASK_STATUS("taskStatus");
+    TASK_STATUS("taskStatus"),
+    CASE_IDS("caseIds"),
+    CASE_TYPE("caseType");
 
     private final String name;
 
---
+++ src/main/java/com/sms/courier/repository/CustomizedScheduleRepository.java
@@ -0,0 +1,9 @@
+package com.sms.courier.repository;
+
+import java.util.List;
+
+public interface CustomizedScheduleRepository {
+
+    Boolean removeCaseIds(List<String> caseIds);
+
+}
