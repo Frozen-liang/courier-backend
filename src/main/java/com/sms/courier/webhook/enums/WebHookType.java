4073714dec0e7c2acc4efcccf892a53b435ca6d5
pader.zhang <pader.zhang@starlight-sms.com>
2021-11-09 08:57:17 +0800

    Add WebhookEvent

---
+++ src/main/java/com/sms/courier/webhook/enums/WebHookType.java
@@ -0,0 +1,14 @@
+package com.sms.courier.webhook.enums;
+
+public enum WebHookType {
+    WORKSPACE,
+    PROJECT,
+    TEST_CASE,
+    API,
+    SCENE_CASE,
+    TEST_CASE_REPORT,
+    SCENE_CASE_REPORT,
+    JOB
+
+
+}
