9a50f8c9a2ad676e9785977d8cd2a5f23f04aa5f
frozen.liang <frozen.liang@starlight-sms.com>
2022-02-17 14:33:50 +0800

    optimize export dataCollection

--- src/test/java/com/sms/courier/service/DataCollectionServiceTest.java
+++ src/test/java/com/sms/courier/service/DataCollectionServiceTest.java
@@ -16,25 +16,39 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.excel.entity.params.ExcelExportEntity;
+import com.sms.courier.common.enums.DataCollection;
 import com.sms.courier.common.exception.ApiTestPlatformException;
 import com.sms.courier.dto.request.DataCollectionImportRequest;
 import com.sms.courier.dto.request.DataCollectionRequest;
 import com.sms.courier.dto.response.DataCollectionResponse;
 import com.sms.courier.entity.datacollection.DataCollectionEntity;
+import com.sms.courier.entity.datacollection.DataParam;
+import com.sms.courier.entity.datacollection.TestData;
 import com.sms.courier.mapper.DataCollectionMapper;
 import com.sms.courier.repository.CustomizedDataCollectionRepository;
 import com.sms.courier.repository.DataCollectionRepository;
 import com.sms.courier.service.impl.DataCollectionServiceImpl;
+
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+
+import org.apache.poi.ss.usermodel.Workbook;
 import org.assertj.core.util.Lists;
 import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.multipart.MultipartFile;
@@ -42,20 +56,37 @@ import org.springframework.web.multipart.MultipartFile;
 @DisplayName("Tests for DataCollectionService")
 class DataCollectionServiceTest {
 
+    private static final Integer SIZE = 10;
     private final DataCollectionRepository dataCollectionRepository = mock(DataCollectionRepository.class);
     private final DataCollectionMapper dataCollectionMapper = mock(DataCollectionMapper.class);
     private final CustomizedDataCollectionRepository customizedDataCollectionRepository = mock(
-        CustomizedDataCollectionRepository.class);
+            CustomizedDataCollectionRepository.class);
     private final ProjectEnvironmentService projectEnvironmentService = mock(ProjectEnvironmentService.class);
     private final DataCollectionService dataCollectionService = new DataCollectionServiceImpl(
-        dataCollectionRepository, dataCollectionMapper, customizedDataCollectionRepository, projectEnvironmentService);
+            dataCollectionRepository, dataCollectionMapper, customizedDataCollectionRepository,
+            projectEnvironmentService);
     private final DataCollectionEntity dataCollection = DataCollectionEntity.builder().id(ID).build();
     private final DataCollectionResponse dataCollectionResponse = DataCollectionResponse.builder().id(ID).build();
     private final DataCollectionRequest dataCollectionRequest = DataCollectionRequest.builder().id(ID).build();
     private static final String ID = ObjectId.get().toString();
     private static final Integer TOTAL_ELEMENTS = 10;
     private static final String COLLECTION_NAME = "collectionName";
+    private static final String PARAM_LIST = "paramList";
+    private static final String KEY = "key";
+    private static final String DATA_NAME = "dataName";
+    private static final String VAULE = "vaule";
     private static final String PROJECT_ID = ObjectId.get().toString();
+
+    private static final MockedStatic<ExcelExportUtil> EXCEL_UTIL_MOCKED_STATIC;
+
+    static {
+        EXCEL_UTIL_MOCKED_STATIC = Mockito.mockStatic(ExcelExportUtil.class);
+    }
+
+    @AfterAll
+    public static void colse() {
+        EXCEL_UTIL_MOCKED_STATIC.close();
+    }
 
     @Test
     @DisplayName("Test the findById method in the DataCollection service")
@@ -82,7 +113,7 @@ class DataCollectionServiceTest {
     public void findById_exception_test() {
         when(dataCollectionRepository.findById(ID)).thenReturn(Optional.empty());
         assertThatThrownBy(() -> dataCollectionService.findById(ID)).isInstanceOf(ApiTestPlatformException.class)
-            .extracting("code").isEqualTo(GET_DATA_COLLECTION_BY_ID_ERROR.getCode());
+                .extracting("code").isEqualTo(GET_DATA_COLLECTION_BY_ID_ERROR.getCode());
     }
 
     @Test
@@ -100,8 +131,8 @@ class DataCollectionServiceTest {
         when(dataCollectionMapper.toEntity(any())).thenReturn(DataCollectionEntity.builder().build());
         doThrow(new RuntimeException()).when(dataCollectionRepository).insert(any(DataCollectionEntity.class));
         assertThatThrownBy(() -> dataCollectionService.add(dataCollectionRequest))
-            .isInstanceOf(ApiTestPlatformException.class)
-            .extracting("code").isEqualTo(ADD_DATA_COLLECTION_ERROR.getCode());
+                .isInstanceOf(ApiTestPlatformException.class)
+                .extracting("code").isEqualTo(ADD_DATA_COLLECTION_ERROR.getCode());
     }
 
     @Test
@@ -120,8 +151,8 @@ class DataCollectionServiceTest {
         when(dataCollectionRepository.existsById(any())).thenReturn(Boolean.TRUE);
         doThrow(new RuntimeException()).when(dataCollectionRepository).save(any(DataCollectionEntity.class));
         assertThatThrownBy(() -> dataCollectionService.edit(dataCollectionRequest))
-            .isInstanceOf(ApiTestPlatformException.class)
-            .extracting("code").isEqualTo(EDIT_DATA_COLLECTION_ERROR.getCode());
+                .isInstanceOf(ApiTestPlatformException.class)
+                .extracting("code").isEqualTo(EDIT_DATA_COLLECTION_ERROR.getCode());
     }
 
     @Test
@@ -130,8 +161,8 @@ class DataCollectionServiceTest {
         when(dataCollectionMapper.toEntity(dataCollectionRequest)).thenReturn(dataCollection);
         when(dataCollectionRepository.existsById(any())).thenReturn(Boolean.FALSE);
         assertThatThrownBy(() -> dataCollectionService.edit(dataCollectionRequest))
-            .isInstanceOf(ApiTestPlatformException.class)
-            .extracting("code").isEqualTo(EDIT_NOT_EXIST_ERROR.getCode());
+                .isInstanceOf(ApiTestPlatformException.class)
+                .extracting("code").isEqualTo(EDIT_NOT_EXIST_ERROR.getCode());
     }
 
     @Test
@@ -156,8 +187,8 @@ class DataCollectionServiceTest {
     public void list_exception_test() {
         doThrow(new RuntimeException()).when(dataCollectionRepository).findAll(any(), any(Sort.class));
         assertThatThrownBy(() -> dataCollectionService.list(PROJECT_ID, COLLECTION_NAME, ""))
-            .isInstanceOf(ApiTestPlatformException.class)
-            .extracting("code").isEqualTo(GET_DATA_COLLECTION_LIST_ERROR.getCode());
+                .isInstanceOf(ApiTestPlatformException.class)
+                .extracting("code").isEqualTo(GET_DATA_COLLECTION_LIST_ERROR.getCode());
     }
 
     @Test
@@ -173,10 +204,10 @@ class DataCollectionServiceTest {
     public void delete_exception_test() {
         List<String> ids = Collections.singletonList(ID);
         doThrow(new RuntimeException()).when(customizedDataCollectionRepository)
-            .deleteByIds(ids);
+                .deleteByIds(ids);
         assertThatThrownBy(() -> dataCollectionService.delete(ids))
-            .isInstanceOf(ApiTestPlatformException.class)
-            .extracting("code").isEqualTo(DELETE_DATA_COLLECTION_BY_ID_ERROR.getCode());
+                .isInstanceOf(ApiTestPlatformException.class)
+                .extracting("code").isEqualTo(DELETE_DATA_COLLECTION_BY_ID_ERROR.getCode());
     }
 
     @Test
@@ -193,8 +224,8 @@ class DataCollectionServiceTest {
     public void getParamList_exception_test() {
         doThrow(new RuntimeException()).when(customizedDataCollectionRepository).getParamListById(any());
         assertThatThrownBy(() -> dataCollectionService.getParamListById(ID))
-            .isInstanceOf(ApiTestPlatformException.class)
-            .extracting("code").isEqualTo(GET_DATA_COLLECTION_PARAM_LIST_BY_ID_ERROR.getCode());
+                .isInstanceOf(ApiTestPlatformException.class)
+                .extracting("code").isEqualTo(GET_DATA_COLLECTION_PARAM_LIST_BY_ID_ERROR.getCode());
     }
 
     @Test
@@ -203,8 +234,8 @@ class DataCollectionServiceTest {
         ClassPathResource classPathResource = new ClassPathResource("/datacollection/template.csv");
         MultipartFile multipartFile = mock(MultipartFile.class);
         DataCollectionImportRequest dataCollectionImportRequest = DataCollectionImportRequest.builder().importMode(1)
-            .file(multipartFile)
-            .build();
+                .file(multipartFile)
+                .build();
         when(dataCollectionRepository.findById(any())).thenReturn(Optional.of(DataCollectionEntity.builder().build()));
         when(multipartFile.getInputStream()).thenReturn(classPathResource.getInputStream());
         when(dataCollectionRepository.save(any())).thenReturn(null);
@@ -215,10 +246,10 @@ class DataCollectionServiceTest {
     @DisplayName("Test the listByEnvId method in the DataCollection service")
     public void listByEnvId_test() {
         List<DataCollectionEntity> entityList =
-            Lists.newArrayList(DataCollectionEntity.builder().envId(ID).build());
+                Lists.newArrayList(DataCollectionEntity.builder().envId(ID).build());
         when(dataCollectionRepository.findAll(any(), any(Sort.class))).thenReturn(entityList);
         List<DataCollectionResponse> responseList =
-            Lists.newArrayList(DataCollectionResponse.builder().envId(ID).build());
+                Lists.newArrayList(DataCollectionResponse.builder().envId(ID).build());
         when(dataCollectionMapper.toDtoList(any())).thenReturn(responseList);
         List<DataCollectionResponse> dto = dataCollectionService.listByEnvIdAndEnvIdIsNull(ID, ID);
         assertThat(dto).isNotEmpty();
@@ -229,7 +260,33 @@ class DataCollectionServiceTest {
     public void listByEnvId_exception_test() {
         when(dataCollectionRepository.findAll(any(), any(Sort.class))).thenThrow(new RuntimeException());
         assertThatThrownBy(() -> dataCollectionService.listByEnvIdAndEnvIdIsNull(ID, ID))
-            .isInstanceOf(ApiTestPlatformException.class);
+                .isInstanceOf(ApiTestPlatformException.class);
     }
 
+    @Test
+    @DisplayName("Test the exportDataCollection method in the DataCollection service")
+    public void exportDataCollection_test(){
+        DataCollectionEntity entity =
+                DataCollectionEntity.builder().collectionName(COLLECTION_NAME).id(ID)
+                        .paramList(Lists.newArrayList(PARAM_LIST))
+                        .dataList(Lists.newArrayList(
+                                TestData.builder().dataName(DATA_NAME).data(
+                                        Lists.newArrayList(DataParam.builder().key(KEY).value(VAULE).build()))
+                                        .build())).build();
+        when(dataCollectionRepository.findById(any())).thenReturn(Optional.of(entity));
+        OutputStream outputStream = mock(OutputStream.class);
+        Workbook workbook = mock(Workbook.class);
+        when(ExcelExportUtil.exportExcel(any(ExportParams.class), any(List.class), any())).thenReturn(workbook);
+        dataCollectionService.exportDataCollection(outputStream, ID);
+        verify(dataCollectionRepository, times(1)).findById(any());
+    }
+
+    @Test
+    @DisplayName("An exception occurred while listByEnvId method in the DataCollection service")
+    public void exportDataCollection_exception_test() {
+        OutputStream outputStream = mock(OutputStream.class);
+        when(dataCollectionRepository.findById(ID)).thenThrow(new RuntimeException());
+        assertThatThrownBy(() -> dataCollectionService.exportDataCollection(outputStream, ID))
+                .isInstanceOf(ApiTestPlatformException.class);
+    }
 }
