4dfb939d4d60451d7590f09833579193de1498b1
wenqiang <wenqiang.yin@starlight-sms.com>
2022-02-28 16:06:42 +0800

    Optimized s3

--- src/main/java/com/sms/courier/repository/impl/CustomizedFileRepositoryImpl.java
+++ src/main/java/com/sms/courier/repository/impl/CustomizedFileRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.sms.courier.repository.impl;
 
 import static com.sms.courier.common.exception.ErrorCode.EDIT_NOT_EXIST_ERROR;
 import static com.sms.courier.common.field.CommonField.ID;
+
 import com.mongodb.client.gridfs.GridFSFindIterable;
 import com.mongodb.client.gridfs.model.GridFSFile;
 import com.sms.courier.entity.file.FileInfoEntity;
@@ -9,7 +10,6 @@ import com.sms.courier.repository.CustomizedFileRepository;
 import com.sms.courier.utils.ExceptionUtils;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -18,7 +18,6 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsResource;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.data.mongodb.gridfs.GridFsUpload;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -45,9 +44,9 @@ public class CustomizedFileRepositoryImpl implements CustomizedFileRepository {
     public String insertTestFile(MultipartFile multipartFile) {
         try {
             ObjectId id = gridFsTemplate.store(
-                    multipartFile.getInputStream(),
-                    multipartFile.getOriginalFilename(),
-                    multipartFile.getContentType());
+                multipartFile.getInputStream(),
+                multipartFile.getOriginalFilename(),
+                multipartFile.getContentType());
             return id.toString();
         } catch (IOException e) {
             throw ExceptionUtils.mpe("Get file InputStream error!");
@@ -57,25 +56,16 @@ public class CustomizedFileRepositoryImpl implements CustomizedFileRepository {
     @Override
     @SuppressFBWarnings("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
     public Boolean updateTestFile(FileInfoEntity fileInfo, MultipartFile multipartFile) {
-        try {
-            String id = fileInfo.getSourceId();
-            Query query = new Query();
-            ID.is(id).ifPresent(query::addCriteria);
-            GridFSFile gridFsFile = gridFsTemplate.findOne(query);
-            if (Objects.isNull(gridFsFile)) {
-                throw ExceptionUtils.mpe(EDIT_NOT_EXIST_ERROR, "TestFile", id.toString());
-            }
-            gridFsTemplate.delete(query);
-            InputStream inputStream = multipartFile.getInputStream();
-            GridFsUpload<String> gridFsUpload = GridFsUpload.fromStream(inputStream)
-                    .id(id).contentType(Objects.requireNonNull(multipartFile.getContentType()))
-                    .filename(Objects.requireNonNull(multipartFile.getOriginalFilename())).build();
-            gridFsTemplate.store(gridFsUpload);
-            return true;
-        } catch (IOException e) {
-            throw ExceptionUtils.mpe("The file is error!");
+        String id = fileInfo.getSourceId();
+        Query query = new Query();
+        ID.is(id).ifPresent(query::addCriteria);
+        GridFSFile gridFsFile = gridFsTemplate.findOne(query);
+        if (Objects.isNull(gridFsFile)) {
+            throw ExceptionUtils.mpe(EDIT_NOT_EXIST_ERROR, "TestFile", id);
         }
-
+        gridFsTemplate.delete(query);
+        fileInfo.setSourceId(insertTestFile(multipartFile));
+        return true;
     }
 
     @Override
